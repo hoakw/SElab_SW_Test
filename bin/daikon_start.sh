b438dbf30616ad109529cde58ad408036e34c897
Su June <hoakw12@gmail.com>
2017-07-06 13:39:03 +0900

    Restart

--- bin/daikon_start.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-#If number of testcase is 300, you have to change code as folloing:
-#for i in {1..300}
-
-for i in {1..100}
-do
-	java daikon.Daikon --config_option daikon.derive.Derivation.disable_derived_variables=true ~/sw/daikon-output/test$i.decls ~/sw/daikon-output/test$i.dtrace
-done
--- build.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-
-## If you want to test other project, change value.
-$project=printtokens  #project dir name
-$version=v1 # project version
-gcc -gdwarf-2 ./$project/versions.alt/versions.orig/$version/print_tokens.c -o print_tokens.exe
-cp -rf ./$project/inputs ./
-gcc ./src/make_input.c -o ./bin/make_input.exe
